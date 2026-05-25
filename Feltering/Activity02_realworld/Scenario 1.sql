cf3193f110378d95083314542fe9a35a7c2af945
johnlouiealibuyog-willow <johnlouiealibuyog05@gmail.com>
2026-05-25 15:31:28 +0800

    Update Scenario 1.sql

--- Feltering/Activity02_realworld/Scenario 1.sql
+++ Feltering/Activity02_realworld/Scenario 1.sql
@@ -1,9 +1,6 @@
---Scenario 1: The warehouse manager needs to know which items are running low. Show all inventory items wherequantity_on_hand is LESS THAN or EQUAL TO the reorder_level. Sort by quantity_on_hand (lowest first).
-SELECT item_name,
-    warehouse,
-    quantity_on_hand,
-    reorder_level
+--Scenario 1: The warehouse manager needs to know which items are running low. Show all inventory items where quantity_on_hand is LESS THAN or EQUAL TO the reorder_level. Sort by quantity_on_hand (lowest first).
+SELECT *
 FROM inventory
 WHERE quantity_on_hand <= reorder_level
-ORDER BY quantity_on_hand 
+ORDER BY quantity_on_hand ASC
 --4 rows returned
